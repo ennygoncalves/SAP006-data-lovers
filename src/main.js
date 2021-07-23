96098ef78825665adef9543e0b6b4f32a8fc0d29
ennygoncalves <ennyligoncalves@gmail.com>
2021-07-22 23:28:45 -0300

    Added hamburger button menu function to activate it

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import { example } from './data.js';
+// // import data from './data/lol/lol.js';
+// import data from './data/pokemon/pokemon.js';
+// // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// console.log(example, data);
+document.getElementById("menu").addEventListener('click', onClickMenu)
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change"); //toggle == "troca" uma class por outra
+	document.getElementById("nav").classList.toggle("change");
+	console.log(document.getElementById("menu-bg").classList)
+	document.getElementById("menu-bg").classList.toggle("change-bg")
+	console.log(document.getElementById("menu-bg").classList)
+}

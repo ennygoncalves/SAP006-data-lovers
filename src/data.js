bffbb8d2701ce361e3f80cbf784b1c5e93e99c84
ennygoncalves <ennyligoncalves@gmail.com>
2021-07-23 22:49:05 -0300

    add sort method to filter the select

--- src/data.js
+++ src/data.js
@@ -1,9 +1,30 @@
-// estas funciones son de ejemplo
+export const ordering = (element, filter) =>{
+  
+  switch (filter){
+    case ("A-Z"): 
+      return element.sort((a, b) => a.title > b.title ? 1 : -1);
 
-export const example = () => {
-  return 'example';
-};
+    case ("Z-A"):
+      return element.sort((a, b) => a.title > b.title ? -1 : -1);
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+    case ("older"):
+      return element.sort((a, b) => parseInt(a.release_date) > parseInt(b.release_date) ? 1 : -1);
+
+    case ("newer"):
+      return element.sort((a, b) => parseInt(a.release_date) > parseInt(b.release_date) ? -1 : 1);
+
+    case ("low"):
+      return element.sort((a, b) => parseInt(a.rt_score) > parseInt(b.rt_score) ? 1 : -1);
+
+    case ("high"):
+      return element.sort((a, b) => parseInt(a.rt_score) > parseInt(b.rt_score) ? -1 : 1);
+
+    case ("internal"):
+      return element.sort((a, b) => parseInt(a.chronological_position) > parseInt(b.chronological_position) ? 1 : -1);
+    
+}
+  
+}
+
+
+

031f4e2243265651c08173545be69ae58bcbb72f
ennygoncalves <ennyligoncalves@gmail.com>
2021-07-25 16:39:42 -0300

    add small tweaks

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-// import { example } from './data.js';
-// // import data from './data/lol/lol.js';
-// import data from './data/pokemon/pokemon.js';
-// // import data from './data/rickandmorty/rickandmorty.js';
 
-// console.log(example, data);
 document.getElementById("menu").addEventListener('click', onClickMenu)
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change"); //toggle == "troca" uma class por outra

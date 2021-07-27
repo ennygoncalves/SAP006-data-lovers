cfee16875ff268d5d2bf5dcd6c2d74cdef4f41dc
ennygoncalves <ennyligoncalves@gmail.com>
2021-07-27 14:14:44 -0300

    linter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
 document.getElementById("menu").addEventListener('click', onClickMenu)
-function onClickMenu(){
+function onClickMenu() {
 	document.getElementById("menu").classList.toggle("change"); //toggle == "troca" uma class por outra
 	document.getElementById("nav").classList.toggle("change");
 	document.getElementById("menu-bg").classList.toggle("change-bg")
